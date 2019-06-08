e7bd29d2794ebdef46762ada3184c82b67232e20
kurodoll <kurodoll@protonmail.ch>
2019-06-09 03:27:48 +1200

    Design login scene game title

--- public/js/Scenes/Login.js
+++ public/js/Scenes/Login.js
@@ -27,9 +27,16 @@ class SceneLogin extends Phaser.Scene {
 
         // Actual assets.
         this.load.image("login bg", "/graphics/art/login_bg");
+
+        // Allow Google fonts to be loaded.
+        this.load.script(
+            "webfont",
+            "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+        );
     }
 
     create() {
+        //================================================================ Text
         // Background image.
         this.background = this.add.image(0, 0, "login bg").setOrigin(0, 0);
         this.background.displayWidth = this.cw;
@@ -43,6 +50,45 @@ class SceneLogin extends Phaser.Scene {
             alpha: {
                 getStart: () => 0,
                 getEnd: () => 1
+            }
+        });
+
+        // Game title.
+        WebFont.load({
+            google: {
+                families: [ "Rajdhani" ]
+            },
+            active: () => {
+                const game_title = this.add.text(
+                    this.cw/2, 100,
+                    "Kimera M.O.E.",
+                    {
+                        fontFamily: "Rajdhani",
+                        fontSize: 100,
+                        color: "#FFFFFF"
+                    }
+                ).setShadow(0, 0, "#000000", 5, false, true).setOrigin(0.5);
+
+                const game_subtitle = this.add.text(
+                    this.cw/2 + 160, 160,
+                    "キメラ萌",
+                    {
+                        fontFamily: "Rajdhani",
+                        fontSize: 30,
+                        color: "#FFFFFF"
+                    }
+                ).setShadow(0, 0, "#000000", 5, false, true).setOrigin(0.5);
+
+                this.add.tween({
+                    targets: [ game_title, game_subtitle ],
+                    ease: "Sine.easeInOut",
+                    duration: 3000,
+                    delay: 0,
+                    alpha: {
+                        getStart: () => 0,
+                        getEnd: () => 1
+                    }
+                });
             }
         });
     }
